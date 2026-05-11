e29b237fb1344c80ba9f6a046190ccbb567fd160
Dr1ftyy <126134710+Dr1ftyy@users.noreply.github.com>
2026-05-11 03:51:49 +0000

    the issue was the indentation, didn't cross my mind when writing out the tabs that it should be inside of main

--- src/pages/admin_view.py
+++ src/pages/admin_view.py
@@ -24,41 +24,42 @@ def main():
     
     st.title("🏥 MedDesk - Admin Panel")
 
-    
+    col1, col2 = st.columns([8,1])
+    with col2:
 
-    #logout button
-    if st.button("Logout", key = "admin_logout"):
-        st.session_state.logged_in = False
-        st.session_state.role = None
-        st.session_state.show_admin_login = False
-        st.switch_page("main.py")
-    
-#st.divider()
+        #logout button
+        if st.button("Logout", key = "admin_logout"):
+            st.session_state.logged_in = False
+            st.session_state.role = None
+            st.session_state.show_admin_login = False
+            st.switch_page("main.py")
+        
+    st.divider()
 
-#tabs for different secionts the admin can look and view
-tab1, tab2, tab3, tab4, tab5 = st.tabs([
-    "Patients",
-    "Appointments",
-    "Billing",
-    "Analytics",
-    "Doctors"
-])
+    #tabs for different secionts the admin can look and view
+    tab1, tab2, tab3, tab4, tab5 = st.tabs([
+        "Patients",
+        "Appointments",
+        "Billing",
+        "Analytics",
+        "Doctors"
+    ])
 
-# Tab 1: Patients
-with tab1:
-    st.subheader("Patient Records")
-# Tab 2: Appointments
-with tab2:
-    st.subheader("All Appointments")
-# Tab 3: Billing
-with tab3:
-    st.subheader("Billing Records")
-# Tab 4: Analytics
-with tab4:
-    st.subheader("Analytics Dashboard")
-# Tab 5: Doctors
-with tab5:
-    st.subheader("Doctor Directory")
+    # Tab 1: Patients
+    with tab1:
+        st.subheader("Patient Records")
+    # Tab 2: Appointments
+    with tab2:
+        st.subheader("All Appointments")
+    # Tab 3: Billing
+    with tab3:
+        st.subheader("Billing Records")
+    # Tab 4: Analytics
+    with tab4:
+        st.subheader("Analytics Dashboard")
+    # Tab 5: Doctors
+    with tab5:
+        st.subheader("Doctor Directory")
 
 if __name__ == "__main__":
     main()
